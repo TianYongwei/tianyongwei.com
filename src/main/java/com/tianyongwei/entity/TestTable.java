867dda34808c0e2fba1e043e7726814a7aa9a44b
tyw <ityongwei@163.com>
2017-11-20 22:24:26 +0800

    up

---
+++ src/main/java/com/tianyongwei/entity/TestTable.java
@@ -0,0 +1,13 @@
+package com.tianyongwei.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table
+@Entity
+public class TestTable {
+
+    private String name;
+
+    private String number;
+}
