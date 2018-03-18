ac658fa72cf6fd3689ce1c499566a6d090901ebe
tianyw <869358189@qq.com>
2018-03-18 14:36:23 +0800

    add cookie utils

--- src/main/java/com/tianyongwei/controller/HelloController.java
+++ src/main/java/com/tianyongwei/controller/HelloController.java
@@ -4,6 +4,8 @@ import com.tianyongwei.utils.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -11,8 +13,17 @@ import javax.servlet.http.HttpSession;
 public class HelloController extends BaseController{
 
     @RequestMapping("/")
-    public String hello(HttpServletResponse response, HttpSession session) {
+    public String hello(HttpServletRequest request,HttpServletResponse response, HttpSession session) {
         session.setAttribute("tyw","anything");
+        System.out.println(session.getId());
+        Cookie cookie = new Cookie("tyw","tywvalue");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+        Cookie cookie1 = new Cookie("ssss","123123123");
+        cookie1.setHttpOnly(true);
+        response.addCookie(cookie1);
+        System.out.println(request.getCookies()[0].getValue());
+        System.out.println(request.getCookies()[1].getValue());
         return "index";
     }
 
